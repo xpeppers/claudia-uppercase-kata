09398f1ef7b63b3cb91105b1b2638702598dcf70
Christian Fei <crifei93@gmail.com>
2016-10-03 14:34:42 +0200

    import code from blogpost as-is

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # claudia-uppercase-kata
 
 Inspiration taken from [this article on claudiajs.com](https://claudiajs.com/tutorials/designing-testable-lambdas.html).
+
+# Setup
+
+## Requirements
+
+- node (tested with Node.js 4.3)
+
+## Instructions
+
+```
+npm install
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+var aws = require('aws-sdk');
+
+exports.handler = function (event, context) {
+  var convert = function(bucket, fileKey) {
+    var s3 = new aws.S3(),
+        Transform = require('stream').Transform,
+        uppercase = new Transform({decodeStrings: false}),
+        stream;
+      uppercase._transform = function (chunk, encoding, done) {
+        done(null, chunk.toUpperCase());
+      };
+      stream = s3.getObject({
+        Bucket: bucket,
+        Key: fileKey
+      }).createReadStream();
+      stream.setEncoding('utf8');
+      stream.pipe(uppercase);
+      s3.upload({
+        Bucket: bucket,
+        Key: fileKey.replace(/^in/, 'out'),
+        Body: uppercase,
+        ACL: 'private'
+      }, context.done);
+    },
+    eventRecord = event.Records && event.Records[0];
+
+  if (eventRecord) {
+    if (eventRecord.eventSource === 'aws:s3' && eventRecord.s3) {
+      convert(
+        eventRecord.s3.bucket.name,
+        eventRecord.s3.object.key
+      );
+    } else {
+      context.fail('unsupported event source');
+    }
+  } else {
+    context.fail('no records in the event');
+  }
+};
