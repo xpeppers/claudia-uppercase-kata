880e9874f2c5f92e4f0235bc5a32d99f4d6879f5
Christian Fei <crifei93@gmail.com>
2016-10-06 15:13:02 +0200

    add acceptance test, remove integration test

--- index_test.js
+++ index_test.js
@@ -2,51 +2,79 @@ var aws = require('aws-sdk');
 var assert = require('assert')
 
 var bucketName = "claudia-uppercase-test"
+var inputFileName = "inputfile.txt"
+var outputFileName = "outputfile.txt"
 
-var event = {
-  "Records": [
-    {
-      "eventSource": "aws:s3",
-      "s3": {
-        "bucket": {
-          "name": bucketName
-        },
-        "object": {
-          "key": "inputfile.txt"
-        }
-      }
-    }
-  ]
-}
-
-var handler = require("./index").handler
 
 describe('Uppercase Bucket Stream', function(){
-  it('saves uppercased output file on S3 Bucket', function(done){
-    this.timeout(15000)
+  it('converts inputfile to uppercase and writes to outputfile', function(){
+    this.timeout(30000)
 
-    handler(event, {
-      done: function(err, data) {
-        assert.equal(data.Bucket, "claudia-uppercase-test")
-        assert.equal(data.Key, "outputfile.txt")
-        assert.equal(data.Location, 'https://claudia-uppercase-test.s3.amazonaws.com/outputfile.txt')
+    var inputContent = "lorem ipsum dolor sit amet."
+    var uppercasedContent = "LOREM IPSUM DOLOR SIT AMET."
 
-        readS3Object(data.Bucket, data.Key, function(err, data) {
-          assert.equal(err, null)
-          assert.equal(data.Body.toString(), "LOREM IPSUM DOLOR SIT AMET.\n")
-          done()
-        })
-      }
+    return deleteS3Object(bucketName, inputFileName)
+    .then(() => deleteS3Object(bucketName, outputFileName))
+    .then(() => putS3Object(bucketName, inputFileName, inputContent))
+    .then(() => waitUntilS3ObjectExists(bucketName, outputFileName))
+    .then((data) => {
+      assert.equal(data.Body.toString(), uppercasedContent)
     })
   })
 })
 
 
-function readS3Object(bucket, key, callback) {
+function deleteS3Object(bucket, key) {
   var s3 = new aws.S3({ signatureVersion: 'v4' })
 
-  var stream = s3.getObject({
-    Bucket: bucket,
-    Key: key
-  }, callback)
+  return new Promise((resolve, reject) => {
+    console.log('-- delete s3 object', bucket, key)
+    s3.deleteObject({
+      Bucket: bucket,
+      Key: key,
+    }, (err, data) => err ? reject(err) : resolve(data) )
+  })
+}
+
+function putS3Object(bucket, key, body) {
+  var s3 = new aws.S3({ signatureVersion: 'v4' })
+  return new Promise((resolve, reject) => {
+    console.log('-- put s3 object', bucket, key)
+    s3.putObject({
+      Bucket: bucket,
+      Key: key,
+      Body: body
+    }, (err, data) => err ? reject(err) : resolve(data) )
+  })
+}
+
+function readS3Object(bucket, key) {
+  var s3 = new aws.S3({ signatureVersion: 'v4' })
+
+  return new Promise((resolve, reject) => {
+    console.log('-- read s3 object', bucket, key)
+    var stream = s3.getObject({
+      Bucket: bucket,
+      Key: key
+    }, (err, data) => err ? reject(err) : resolve(data) )
+  })
+}
+
+function waitUntilS3ObjectExists(bucket, key) {
+  return new Promise((resolve, reject) => {
+    var retries = 0
+    var intervalId = setInterval(() => {
+      console.log('-- waiting until s3 object exists', bucket, key)
+      readS3Object(bucket, key)
+      .then(resolve)
+      .catch(() => {
+        console.log('-- retries', retries)
+        if(retries > 10) {
+          reject(new Error(`file (${key}) not found in bucket (${bucket})`))
+          clearInterval(intervalId)
+        }
+      })
+      retries++
+    }, 1000)
+  })
 }
